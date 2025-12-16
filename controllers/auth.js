294d3813ac396978b122cd113950113bf07d111f
vikramgopalan-gaseb <vikram.r.gopalan@gmail.com>
2025-12-15 18:49:50 -0700

    12/15 v3

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,7 @@
+const bcrypt = require("bcrypt");
+
+const User = require("../models/user.js");
+
 const express = require("express");
 const router = express.Router();
 
@@ -6,3 +10,24 @@ module.exports = router;
 router.get("/sign-up", (req, res) => {
   res.render("auth/sign-up.ejs");
 });
+
+router.post("/sign-up", async (req, res) => {
+  res.send("Form submission accepted!");
+});
+
+const userInDatabase = await User.findOne({ username: req.body.username });
+if (userInDatabase) {
+  return res.send("Username already taken.");
+}
+
+if (req.body.password !== req.body.confirmPassword) {
+  return res.send("Password and Confirm Password must match");
+}
+
+const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+req.body.password = hashedPassword;
+
+// validation logic
+
+const user = await User.create(req.body);
+res.send(`Thanks for signing up ${user.username}`);
