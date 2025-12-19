0c97094f4e9d1d8d796b17ade01d8750bc1cdf3b
vikramgopalan-gaseb <vikram.r.gopalan@gmail.com>
2025-12-18 18:16:43 -0700

    12/18

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,17 +12,18 @@ router.get("/sign-up", (req, res) => {
 });
 
 router.post("/sign-up", async (req, res) => {
+  const userInDatabase = await User.findOne({ username: req.body.username });
+  if (userInDatabase) {
+    return res.send("Username already taken.");
+  }
+  
+  if (req.body.password !== req.body.confirmPassword) {
+    return res.send("Password and Confirm Password must match");
+  }
   res.send("Form submission accepted!");
-});
+}); // review the auth lab to fix this block of code (hint: how to save this to DB)
 
-const userInDatabase = await User.findOne({ username: req.body.username });
-if (userInDatabase) {
-  return res.send("Username already taken.");
-}
 
-if (req.body.password !== req.body.confirmPassword) {
-  return res.send("Password and Confirm Password must match");
-}
 
 const hashedPassword = bcrypt.hashSync(req.body.password, 10);
 req.body.password = hashedPassword;
